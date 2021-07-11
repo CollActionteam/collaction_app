14408d9e4b40654dbd963febd22665c01c78f8f4
Mascha van der Marel <43924750+MaschavanderMarel@users.noreply.github.com>
2021-07-11 18:05:08 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/org/collaction/collaction_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.collaction.collaction_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to CollAction',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to CollAction'),
+          backgroundColor: Color(0xff23d884),
+        ),
+        body: const Center(
+          child: Text('Hello user!'),
+        ),
+      ),
+    );
+  }
+}
