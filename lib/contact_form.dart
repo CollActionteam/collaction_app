fc0a79185aa11fb9ad17136be331c08c6b695346
Mascha van der Marel <43924750+MaschavanderMarel@users.noreply.github.com>
2021-08-01 10:58:34 +0200

    Add test 'Form can be submitted'

--- lib/contact_form.dart
+++ lib/contact_form.dart
@@ -79,8 +79,9 @@ request for starting a crowd action''',
                         // If the form is valid, display a snackbar. In the real world,
                         // you'd often call a server or save the information in a database.
                         ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Processing Data')),
+                          const SnackBar(content: Text('Processing data')),
                         );
+                        // TODO implement connection to microservice
                       }
                     },
                     child: const Text('Submit'),
--- test/contact_form_test.dart
+++ test/contact_form_test.dart
@@ -5,22 +5,44 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:collaction_app/main.dart';
 
 void main() {
-   testWidgets('Contact form widget test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+   testWidgets('Contact form renders', (WidgetTester tester) async {
+    // Build our app and contact form.
     await tester.pumpWidget(MyApp());
-
-    // Tap the button and trigger a page transition
     await tester.tap(find.text('Give feedback or start crowd action'));
     await tester.pumpAndSettle();
 
-    // Verify that the page has changed.
-   final finder = find.text("Contact form");
-   expect(finder, findsOneWidget);
+    // Verify that the page is rendered with all its elements.
+   // final finder = find.text("Contact form");
+   expect(find.text("Contact form"), findsOneWidget);
+   expect(find.byType(TextFormField), findsNWidgets(2));
+   expect(find.text("Submit"), findsOneWidget);
+  });
+
+   testWidgets('Form can be submitted', (WidgetTester tester) async {
+     // Build app and contact form
+     await tester.pumpWidget(MyApp());
+     await tester.tap(find.text('Give feedback or start crowd action'));
+     await tester.pumpAndSettle();
+
+     final Finder emailAddress = find.widgetWithText(TextFormField, 'Your email address');
+     final Finder feedback = find.widgetWithText(TextFormField, 'Your feedback or request');
+     final Finder submit = find.widgetWithText(ElevatedButton, 'Submit');
+
+     expect(find.text('Processing data'), findsNothing);
+
+     await tester.enterText(emailAddress, 'example@mail.com');
+     await tester.enterText(feedback, 'blablabla');
+
+     await tester.tap(submit);
+     await tester.pump();
+
+     expect(find.text('Processing data'), findsOneWidget);
 
   });
 }
