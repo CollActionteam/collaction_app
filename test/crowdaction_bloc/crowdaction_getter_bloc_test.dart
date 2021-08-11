ab1de120d253e8f43a06eed4fffd24fd825fc615
Mathias M <mathias@collaction.org>
2021-08-11 21:34:46 +0200

    feat(tests): add tests for crowdaction_repository (mocked)

--- test/crowdaction_bloc/crowdaction_getter_bloc_test.dart
+++ test/crowdaction_bloc/crowdaction_getter_bloc_test.dart
@@ -1,41 +1,49 @@
 import 'package:collaction_app/application/crowdaction_getter/crowdaction_getter_bloc.dart';
+import 'package:collaction_app/domain/crowdaction/crowdaction.dart';
 import 'package:collaction_app/infrastructure/crowdaction/crowdaction_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
+import 'package:mocktail/mocktail.dart';
+
+class MockClient extends Mock implements http.Client {}
 
 void main() {
-  group('CrowdAction Getter Bloc', () {
-    CrowdActionGetterBloc? crowdActionGetterBloc;
+  // MOCKED CROWDACTION REPOSITORY
+  final client = MockClient();
+  final CrowdActionRepository crowdActionRepository =
+      CrowdActionRepository(client);
 
-    // Initialize Bloc, Repository and HTTP Client
-    setUp(() {
-      crowdActionGetterBloc =
-          CrowdActionGetterBloc(CrowdActionRepository(http.Client()));
-    });
+  group('CrowdAction Getter Bloc:', () {
+    // Setup Mocked Repository
+    when(() => crowdActionRepository.getCrowdActions())
+        .thenAnswer((_) async => crowdactions);
 
-    // Test state on Bloc creation
-    test('Initial State for our Bloc', () {
-      crowdActionGetterBloc!.state
-          .maybeWhen(initial: () => true, orElse: () => false);
-    });
+    final crowdActionGetterBloc = CrowdActionGetterBloc(crowdActionRepository);
 
-    // Test fetching of ALL crowdactions
-    test('Fetch crowdactions', () {
-      crowdActionGetterBloc!.add(const CrowdActionGetterEvent.getMore(null));
-
-      crowdActionGetterBloc!.state
-          .maybeWhen(orElse: () => false, fetched: (result) => true);
-    });
-
-    // Test fetching of fixed amount of crowdactions
-    test('Fetch fixed amount of crowdactions', () {
-      const amountToFetch = 3;
-      crowdActionGetterBloc!
-          .add(const CrowdActionGetterEvent.getMore(amountToFetch));
-
-      crowdActionGetterBloc!.state.maybeWhen(
-          orElse: () => false,
-          fetched: (result) => result.length == amountToFetch);
+    // TODO: Implement tests for BLOC that uses generated code
+    test('State for our Bloc', () async {
+      crowdActionGetterBloc.add(const CrowdActionGetterEvent.getMore(null));
     });
   });
 }
+
+final List<CrowdAction> crowdactions = [
+  CrowdAction(
+      name: 'Dansen bij Pakhuis de Règâh op 9 feb',
+      description:
+          'Misschien wel het belangrijkste collectieve actie probleem dat er is: dansen op een feestje ;). Het is voor iedereen leuker als iedereen gewoon lekker meedanst, maar je hebt nou eenmaal een kritieke massa nodig om het feestje lekker op gang te krijgen. Als 10 mensen nou toezeggen om lekker te gaan dansen vanavond, dan hebben we die massa wel. Doe je mee?',
+      start: DateTime.parse('2018-02-09T00:00:00+00:00'),
+      end: DateTime.parse('2018-02-10T23:59:59+00:00')),
+  CrowdAction(
+      name: 'Earth Hour candlelight dinner voor bedrijven',
+      description:
+          '24 maart is het weer Earth Hour! Wereldwijd gaat in meer dan 185 landen van 20.30 tot 21.30 op heel veel plekken het licht uit om aandacht te vragen voor de kwetsbare natuur op onze aarde. Van de Eiffeltoren in Parijs tot het Opera House in Sidney, en de Erasmusbrug in Rotterdam tot de Martinitoren in Groningen. Doe ook mee! Breng Earth Hour onder de aandacht bij uw medewerkers en relaties en doe met uw bedrijf mee aan onze actie om met zoveel mogelijk Nederlanders een candlelight dinner te organiseren.',
+      start: DateTime.parse('2018-03-08T00:00:00+00:00'),
+      end: DateTime.parse('2018-03-24T23:59:59+00:00')),
+  CrowdAction(
+      name: "It's getting hot in here - Amsterdam edition",
+      description:
+          "<p>Fast Fashion is one of the biggest polluting industries in the world and - among other things - one of the biggest contributors to climate change. So, it's time to stand up to Fast Fashion and raise awareness for our Slow Fashion Summer. It's time to take off all our clothes because being naked is the most sustainable option - the Slow Fashion summer is 2nd :)</p>",
+      start: DateTime.parse('2018-06-11T00:00:00+00:00'),
+      end: DateTime.parse('2018-06-16T23:59:59+00:00')),
+];
