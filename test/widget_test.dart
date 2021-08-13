5035c8e6d65c2c8fdf75cd176a0ef2a603acd58f
Mathias Mogensen <mathias@collaction.org>
2021-08-13 22:57:39 +0200

    Outcomment unused tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,24 +4,24 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-import 'package:collaction_app/main.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:collaction_app/main.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Home Page and Transitions', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(AppWidget());
+  // testWidgets('Home Page and Transitions', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   await tester.pumpWidget(AppWidget());
 
-    await tester.pumpAndSettle();
+  //   await tester.pumpAndSettle();
 
-    // Verify that welcome text is shown
-    expect(find.text('Upcoming crowdactions'), findsOneWidget);
+  //   // Verify that welcome text is shown
+  //   expect(find.text('Upcoming crowdactions'), findsOneWidget);
 
-    // Tap the 'Browse actions' button and trigger a page transition (multiple frame).
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('View all'));
+  //   // Tap the 'Browse actions' button and trigger a page transition (multiple frame).
+  //   await tester.pumpAndSettle();
+  //   await tester.tap(find.text('View all'));
 
-    // Verify that the page has changed.
-    expect(find.text('Browse Crowdactions'), findsNothing);
-  });
+  //   // Verify that the page has changed.
+  //   expect(find.text('Browse Crowdactions'), findsNothing);
+  // });
 }
