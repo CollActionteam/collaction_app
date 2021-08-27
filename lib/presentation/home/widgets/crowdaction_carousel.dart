39e9cbd526caef87f2cede285d6b01beb968b6f4
Mathias Mogensen <mathiasrieckm@gmail.com>
2021-08-28 00:24:31 +0200

    fix(ripple): remove ripple from carousel

--- lib/presentation/home/widgets/crowdaction_carousel.dart
+++ lib/presentation/home/widgets/crowdaction_carousel.dart
@@ -6,6 +6,7 @@ import '../../../application/crowdaction_getter/crowdaction_getter_bloc.dart';
 import '../../../infrastructure/core/injection.dart';
 import '../../shared_widgets/accent_action_chip.dart';
 import '../../shared_widgets/crowdaction_card.dart';
+import '../../shared_widgets/no_ripple_behavior.dart';
 import '../../shared_widgets/secondary_chip.dart';
 import '../../themes/constants.dart';
 
@@ -45,25 +46,28 @@ class _CrowdActionCarouselState extends State<CrowdActionCarousel> {
               SizedBox(
                 width: MediaQuery.of(context).size.width,
                 height: 400.0,
-                child: PageView.builder(
-                  controller: _pageController,
-                  itemCount: 3,
-                  itemBuilder: (context, index) => CrowdActionCard(
-                    title: crowdActions[index].name,
-                    imagePath:
-                        'https://collaction-production.s3.eu-central-1.amazonaws.com/7d50f62a-a73d-401e-a1d8-33a9e8817870.jpg', // TODO: Use crowdaction image
-                    chips: [
-                      // TODO: Use crowdaction chips
-                      AccentActionChip(
-                        () {},
-                        text: 'Sign up now',
-                        leading: const Image(
-                            image:
-                                AssetImage('assets/images/icons/add_icon.png')),
-                      ),
-                      const SecondaryChip(text: 'Sustainability'),
-                      const SecondaryChip(text: 'Environment')
-                    ],
+                child: ScrollConfiguration(
+                  behavior: NoRippleBehavior(),
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount: 3,
+                    itemBuilder: (context, index) => CrowdActionCard(
+                      title: crowdActions[index].name,
+                      imagePath:
+                          'https://collaction-production.s3.eu-central-1.amazonaws.com/7d50f62a-a73d-401e-a1d8-33a9e8817870.jpg', // TODO: Use crowdaction image
+                      chips: [
+                        // TODO: Use crowdaction chips
+                        AccentActionChip(
+                          () {},
+                          text: 'Sign up now',
+                          leading: const Image(
+                              image: AssetImage(
+                                  'assets/images/icons/add_icon.png')),
+                        ),
+                        const SecondaryChip(text: 'Sustainability'),
+                        const SecondaryChip(text: 'Environment')
+                      ],
+                    ),
                   ),
                 ),
               ),
