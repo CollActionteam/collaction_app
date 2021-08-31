ddbc9b64e51bee4a1dc5f251ab15fadced1640ae
Ruben Horn <horn.ruben@yahoo.de>
2021-08-31 20:36:06 +0200

    Only show demo content in debug mode

--- lib/presentation/shared_widgets/menu_drawer.dart
+++ lib/presentation/shared_widgets/menu_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 
@@ -17,6 +18,68 @@ class MenuDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var drawerItems = <Widget>[
+      const Padding(
+        padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15),
+        child: Text(
+          'Menu',
+          style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),
+        ),
+      ),
+      ListTile(
+        onTap: () {},
+        leading: const Icon(Icons.group_add, size: 27),
+        title: const Text('Invite friends'),
+      ),
+      const Divider(
+        color: Colors.black38,
+        height: 1,
+      ),
+      ListTile(
+        onTap: () => pushRoute(context, const CrowdActionBrowseRoute()),
+        leading: const ImageIcon(
+          AssetImage('assets/images/icons/browse.png'),
+          size: 24,
+        ),
+        title: const Text('Browse'),
+      ),
+      ListTile(
+        onTap: () {}, // TODO: Own crowdactions / participating in ...
+        leading: const ImageIcon(
+          AssetImage('assets/images/icons/check.png'),
+          size: 24,
+        ),
+        title: const Text('My Crowdactions'),
+      ),
+      ListTile(
+        onTap: () {}, // TODO: Add profile route
+        leading: const CircleAvatar(
+          backgroundColor: Colors.black,
+          maxRadius: 15,
+          child: Icon(
+            Icons.person,
+            color: Colors.white,
+            size: 20,
+          ),
+        ),
+        title: const Text('Profile'),
+      ),
+      ListTile(
+        onTap: () => pushRoute(context, const ContactFormRoute()),
+        leading: const Icon(Icons.mail_outline_outlined, size: 27),
+        title: const Text('Contact Us'),
+      )
+    ];
+
+    if (!kReleaseMode) {
+      // Demo content should not be visible in release builds
+      drawerItems.add(ListTile(
+        onTap: () => pushRoute(context, const DemoRoute()),
+        leading: const Icon(Icons.list_alt, size: 27),
+        title: const Text('Demo Content'),
+      ));
+    }
+
     return AdvancedDrawer(
       backdropColor: Colors.white,
       controller: _advancedDrawerController,
@@ -24,63 +87,7 @@ class MenuDrawer extends StatelessWidget {
         textColor: Colors.black,
         iconColor: Colors.black,
         child: ListView(
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15),
-              child: Text(
-                'Menu',
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: const Icon(Icons.group_add, size: 27),
-              title: const Text('Invite friends'),
-            ),
-            const Divider(
-              color: Colors.black38,
-              height: 1,
-            ),
-            ListTile(
-              onTap: () => pushRoute(context, const CrowdActionBrowseRoute()),
-              leading: const ImageIcon(
-                AssetImage('assets/images/icons/browse.png'),
-                size: 24,
-              ),
-              title: const Text('Browse'),
-            ),
-            ListTile(
-              onTap: () {}, // TODO: Own crowdactions / participating in ...
-              leading: const ImageIcon(
-                AssetImage('assets/images/icons/check.png'),
-                size: 24,
-              ),
-              title: const Text('My Crowdactions'),
-            ),
-            ListTile(
-              onTap: () {}, // TODO: Add profile route
-              leading: const CircleAvatar(
-                backgroundColor: Colors.black,
-                maxRadius: 15,
-                child: Icon(
-                  Icons.person,
-                  color: Colors.white,
-                  size: 20,
-                ),
-              ),
-              title: const Text('Profile'),
-            ),
-            ListTile(
-              onTap: () => pushRoute(context, const ContactFormRoute()),
-              leading: const Icon(Icons.mail_outline_outlined, size: 27),
-              title: const Text('Contact Us'),
-            ),
-            ListTile(
-              onTap: () => pushRoute(context, const DemoRoute()),
-              leading: const Icon(Icons.list_alt, size: 27),
-              title: const Text('Demo Content'),
-            ),
-          ],
+          children: drawerItems,
         ),
       ),
       child: child,
