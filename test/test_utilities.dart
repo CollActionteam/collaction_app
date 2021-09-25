0f81f4b94af2eed366d6c592a60e7b34e690af7f
Ruben Horn <horn.ruben@yahoo.de>
2021-09-25 02:01:18 +0200

    Fixed unrelated test

--- test/test_utilities.dart
+++ test/test_utilities.dart
@@ -1,9 +1,11 @@
+import 'package:collaction_app/domain/i_settings_repository.dart';
 import 'package:collaction_app/domain/user/i_user_repository.dart';
 import 'package:collaction_app/domain/user/user.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockUserRepository extends Mock implements IUserRepository {}
+class MockSettingsRepository extends Mock implements ISettingsRepository {}
 
 // ignore: avoid_classes_with_only_static_members
 class TestUtilities {
@@ -12,5 +14,12 @@ class TestUtilities {
     when(() => mockUserRepository.observeUser()).thenAnswer((_) => userStream);
     GetIt.I.allowReassignment = true;
     GetIt.I.registerSingleton<IUserRepository>(mockUserRepository);
+  }
+
+  static void mockSettings({ bool wasUserOnboarded = false }) {
+    final mockSettingsRepository = MockSettingsRepository();
+    when(() => mockSettingsRepository.wasUserOnboarded).thenReturn(wasUserOnboarded);
+    GetIt.I.allowReassignment = true;
+    GetIt.I.registerSingleton<ISettingsRepository>(mockSettingsRepository);
   }
 }
