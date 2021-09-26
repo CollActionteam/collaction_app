6da23fa2455973585273ee52d6c9a7733ab59253
Saksham Gupta <sakshamgupta391@gmail.com>
2021-09-26 22:10:39 +0530

    feat: bottom navigation bar
    Relates: CAN-64

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,13 +1,16 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:collaction_app/presentation/demo/demo_screen.dart';
+import 'package:collaction_app/presentation/home/demo_tab_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../domain/i_settings_repository.dart';
 import '../../infrastructure/core/injection.dart';
 import '../routes/app_routes.gr.dart';
-import '../shared_widgets/menu_drawer.dart';
-import '../themes/constants.dart';
-import 'widgets/crowdaction_carousel.dart';
+import 'home_page.dart';
+// import '../themes/constants.dart';
+// import 'widgets/crowdaction_carousel.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -23,64 +26,93 @@ class _HomePageState extends State<HomePage> {
     showOnboarding();
   }
 
+  int index = 0;
+  Widget? pageToShow;
   @override
   Widget build(BuildContext context) {
-    final _advancedDrawerController = AdvancedDrawerController();
-
-    return MenuDrawer(
-      advancedDrawerController: _advancedDrawerController,
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 14.0),
-            child: IconButton(
-              icon: const Image(
-                  image: AssetImage('assets/images/icons/menu_icon.png')),
-              onPressed: () => _advancedDrawerController.showDrawer(),
-            ),
-          ),
+    if (index == 0) {
+      pageToShow = const HomePageScreen();
+    } else if (index == 1) {
+      pageToShow = const DemoTabPage();
+    } else if (index == 2) {
+      pageToShow = const DemoTabPage();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 23.0),
+          child: pageToShow,
+          // Column(
+          //   children: [
+          //     Expanded(
+          //       child: SingleChildScrollView(
+          //         child: Column(
+          //           children: [
+          //             Row(
+          //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //               children: [
+          //                 const Text(
+          //                   'Upcoming crowdactions',
+          //                   style: TextStyle(
+          //                       fontWeight: FontWeight.w500, fontSize: 18.0),
+          //                 ),
+          //                 TextButton(
+          //                   onPressed: () => context.router
+          //                       .push(const CrowdActionBrowseRoute()),
+          //                   child: const Text(
+          //                     'View all',
+          //                     style: TextStyle(
+          //                         fontWeight: FontWeight.w500,
+          //                         fontSize: 14.0,
+          //                         color: kIrisColor),
+          //                   ),
+          //                 ),
+          //               ],
+          //             ),
+          //             const CrowdActionCarousel(),
+          //           ],
+          //         ),
+          //       ),
+          //     ),
+          //   ],
+          // ),
         ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 23.0),
-            child: Column(
-              children: [
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text(
-                              'Upcoming crowdactions',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w500, fontSize: 18.0),
-                            ),
-                            TextButton(
-                              onPressed: () => context.router
-                                  .push(const CrowdActionBrowseRoute()),
-                              child: const Text(
-                                'View all',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 14.0,
-                                    color: kIrisColor),
-                              ),
-                            ),
-                          ],
-                        ),
-                        const CrowdActionCarousel(),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 75,
+        child: BottomNavigationBar(
+            currentIndex: index,
+            backgroundColor: Colors.white,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            selectedItemColor: const Color(0xFF2EB494),
+            unselectedItemColor: const Color(0xFF333333),
+            type: BottomNavigationBarType.fixed,
+            onTap: (value) {
+              if (value != index) {
+                setState(() {
+                  index = value;
+                });
+              }
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/black_logo.png')),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_sharp),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_sharp),
+                label: '',
+              )
+            ]),
       ),
     );
   }
