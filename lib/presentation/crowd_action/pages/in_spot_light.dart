1babd2900259a85a4f317c89d923e4608533df1c
Isaac Obella <wizlif@users.noreply.github.com>
2021-11-15 06:32:29 +0300

    feat(crowdactions): Implement new designs. (#50)
    * feat(crowdactions): Implement new designs.
    
    Signed-off-by: Isaac Obella <wizlif.144@gmail.com>
    
    * feat(crowdactions): Implement new designs.
    
    Signed-off-by: Isaac Obella <wizlif.144@gmail.com>

---
+++ lib/presentation/crowd_action/pages/in_spot_light.dart
@@ -0,0 +1,25 @@
+import 'package:collaction_app/presentation/crowd_action/pages/widgets/share_collaction_card.dart';
+import 'package:flutter/material.dart';
+
+import 'widgets/in_spotlight_header.dart';
+
+class InSpotLightPage extends StatelessWidget {
+  const InSpotLightPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: const [
+              InSpotLightHeader(),
+              ShareCollActionCard(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/themes/themes.dart
+++ lib/presentation/themes/themes.dart
@@ -4,33 +4,18 @@ import 'package:flutter/material.dart';
 import 'constants.dart';
 
 ThemeData lightTheme(BuildContext context) {
-  return ThemeData.light().copyWith(
+  final theme = ThemeData.light();
+
+  return theme.copyWith(
     primaryColor: kPrimaryColor,
-    accentColor: kAccentColor,
+    colorScheme: theme.colorScheme.copyWith(secondary: kAccentColor),
     primaryColorBrightness: Brightness.light,
     scaffoldBackgroundColor: Colors.white,
-    textTheme: Theme.of(context).textTheme.apply(
-          fontFamily: 'Rubik',
-          bodyColor: Colors.black,
-          displayColor: Colors.black,
-        ),
-    accentTextTheme: Theme.of(context)
-        .accentTextTheme
-        .copyWith(
-          button: const TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 15.0,
-          ),
-          caption: const TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 11.0,
-          ),
-        )
-        .apply(
-          fontFamily: 'Rubik',
-          displayColor: Colors.white,
-          bodyColor: Colors.white,
-        ),
+    textTheme: theme.textTheme.apply(
+      fontFamily: 'Rubik',
+      bodyColor: Colors.black,
+      displayColor: Colors.black,
+    ),
     buttonTheme: const ButtonThemeData(
       buttonColor: kPrimaryColor,
     ),
