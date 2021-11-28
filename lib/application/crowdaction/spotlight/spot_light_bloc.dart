42b8b9f48bdc560a50d179ac107d34f32c43ddb0
Isaac Obella <wizlif@users.noreply.github.com>
2021-11-28 21:00:07 +0300

    feat(crowd-action): Spot light bloc.

---
+++ lib/application/crowdaction/spotlight/spot_light_bloc.dart
@@ -0,0 +1,38 @@
+
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../../domain/crowdaction/crowdaction.dart';
+import '../../../domain/crowdaction/crowdaction_failures.dart';
+import '../../../domain/crowdaction/i_crowdaction_repository.dart';
+
+part 'spot_light_bloc.freezed.dart';
+part 'spot_light_event.dart';
+part 'spot_light_state.dart';
+
+@injectable
+class SpotLightBloc extends Bloc<SpotLightEvent, SpotLightState> {
+  final ICrowdActionRepository _crowdActionRepository;
+
+  SpotLightBloc(this._crowdActionRepository) : super(const SpotLightState.initial());
+
+  @override
+  Stream<SpotLightState> mapEventToState(SpotLightEvent event)async* {
+    yield* event.map(getSpotLightCrowdActions: _mapGetSpotLightCrowdActionsToState);
+  }
+
+Stream<SpotLightState> _mapGetSpotLightCrowdActionsToState(
+    _GetSpotlightCrowdActions value) async* {
+  yield const SpotLightState.fetchingCrowdSpotLightActions();
+
+  final spotlightCrowdActionsResult =
+      await _crowdActionRepository.getSpotLightCrowdActions();
+
+  yield spotlightCrowdActionsResult.fold(
+    (failure) => SpotLightState.spotLightCrowdActionsError(failure),
+    (spotLightCrowdActions) =>
+        SpotLightState.spotLightCrowdActions(spotLightCrowdActions),
+  );
+}
+}
--- lib/domain/crowdaction/i_crowdaction_repository.dart
+++ lib/domain/crowdaction/i_crowdaction_repository.dart
@@ -5,6 +5,7 @@ import 'crowdaction_failures.dart';
 
 abstract class ICrowdActionRepository {
   Future<List<CrowdAction>> getCrowdActions({int amount = 0});
+  Future<Either<CrowdActionFailure,List<CrowdAction>>> getSpotLightCrowdActions();
 
   Future<Either<CrowdActionFailure,Unit>> subscribeToCrowdAction(CrowdAction crowdAction);
 }
