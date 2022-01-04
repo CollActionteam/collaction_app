ac07e14d3f97d6d0c7211d31aa17f07d11228ecc
Isaac Obella <wizlif@users.noreply.github.com>
2022-01-04 14:41:59 +0300

    fix: Hide profile tabs. (#128)
    * fix: Hide profile tabs.
    
    * fix: Hide profile tabs.

--- lib/presentation/profile/profile_screen.dart
+++ lib/presentation/profile/profile_screen.dart
@@ -292,7 +292,9 @@ class _UserProfilePageState extends State<UserProfilePage> {
                         ],
                       ),
                     ),
-                    UserProfileTab(user: state.userProfile?.user),
+                    Offstage(
+                      child: UserProfileTab(user: state.userProfile?.user),
+                    ),
                   ],
                 ),
               ),
