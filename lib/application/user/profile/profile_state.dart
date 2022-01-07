ea391d60fbf452305e80d0206c75ec96bfa20ab8
Ruben Horn <5367484+rubenhorn@users.noreply.github.com>
2022-01-07 17:59:03 +0100

    MVP rc2

--- lib/application/user/profile/profile_state.dart
+++ lib/application/user/profile/profile_state.dart
@@ -4,23 +4,29 @@ class ProfileState {
   const ProfileState({
     required this.userProfile,
     this.isEditing,
+    this.wasProfilePictureUpdated,
   });
 
   final UserProfile? userProfile;
   final bool? isEditing;
+  final bool? wasProfilePictureUpdated;
 
   factory ProfileState.initial() => const ProfileState(
         userProfile: null,
         isEditing: false,
+        wasProfilePictureUpdated: false,
       );
 
   ProfileState copyWith({
     UserProfile? userProfile,
     bool? isEditing,
+    bool? wasProfilePictureUpdated,
   }) {
     return ProfileState(
       userProfile: userProfile ?? this.userProfile,
       isEditing: isEditing ?? this.isEditing,
+      wasProfilePictureUpdated:
+          wasProfilePictureUpdated ?? false, // Reset state implicitly
     );
   }
 }
