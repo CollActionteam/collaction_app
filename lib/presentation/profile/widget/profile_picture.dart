06eeb1fb7b3d1a32f19e13ed4cbf4bc1c9f1f4c4
Saksham Gupta <sakshamgupta391@gmail.com>
2022-04-08 00:41:09 +0530

    removing unused package

--- lib/presentation/profile/widget/profile_picture.dart
+++ lib/presentation/profile/widget/profile_picture.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:collaction_app/presentation/themes/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
