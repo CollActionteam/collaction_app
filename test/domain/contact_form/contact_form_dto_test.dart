aab64225a7d0625c733eca479ec03360f0591e0d
Saksham Gupta <sakshamgupta391@gmail.com>
2022-06-04 16:17:40 +0530

    added fixtures for final values and removed redundant tests

--- test/domain/contact_form/contact_form_dto_test.dart
+++ test/domain/contact_form/contact_form_dto_test.dart
@@ -1,22 +1,23 @@
 import 'package:collaction_app/domain/contact_form/contact_form_dto.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'contact_form_fixture.dart';
 
 void main() {
-  late ContactFormDto tContactForm;
-  setUp(() {
-    tContactForm =
-        ContactFormDto(email: 'test@test.com', message: 'Testing DTO');
-  });
-  test('testing Contact form DTO', () async {
-    expect(
-      ContactFormDto(email: 'test@test.com', message: 'Testing DTO'),
-      tContactForm,
-    );
-  });
+  group('Testing Contact form DTO', () {
+    test('testing Contact form DTO', () async {
+      expect(
+        ContactFormDto(email: 'test@test.com', message: 'Testing DTO'),
+        tContactForm,
+      );
+    });
 
-  test('testing ContactFormDto.fromJson() method', () async {
-    final jsonMap = {'email': 'test@test.com', 'message': 'Testing fromJson()'};
-    final result = ContactFormDto.fromJson(jsonMap);
-    expect(result, isA<ContactFormDto>());
+    test('testing ContactFormDto.fromJson() method', () async {
+      final jsonMap = {
+        'email': 'test@test.com',
+        'message': 'Testing fromJson()'
+      };
+      final result = ContactFormDto.fromJson(jsonMap);
+      expect(result, isA<ContactFormDto>());
+    });
   });
 }
