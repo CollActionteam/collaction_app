7de826f42b69613cde48f0efe1edb18bbb4d9553
Saksham Gupta <55953830+glitchpop-frenzy@users.noreply.github.com>
2022-06-07 17:00:15 +0530

    Update build_information_test.dart

--- test/domain/settings/build_information_test.dart
+++ test/domain/settings/build_information_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Test for BuildInformation() constructor', () {
-    const BuildInformation tBuildInfo =
+    const tBuildInfo =
         BuildInformation(buildNumber: 'tBuildNo', version: 'tVersion');
     expect(tBuildInfo.buildNumber, 'tBuildNo');
     expect(tBuildInfo.version, 'tVersion');
