15bebd403e2277b0557adee232c22dc51bffedbc
Saksham Gupta <sakshamgupta391@gmail.com>
2022-06-09 14:37:43 +0530

    100% coverage for application/contact_form_bloc

---
+++ test/application/contact_form/contact_form_fixture.dart
@@ -0,0 +1,13 @@
+import 'package:collaction_app/application/contact_form/contact_form_bloc.dart';
+import 'package:collaction_app/domain/contact_form/contact_form_dto.dart';
+
+import '../../test_utilities.dart';
+
+final tContactFormApi = MockContactFormApi();
+final tContactFormBloc = ContactFormBloc(tContactFormApi);
+final tContactFormDtoS =
+    ContactFormDto(email: 'success@test.com', message: 'Success');
+final tContactFormDtoF =
+    ContactFormDto(email: 'failure@test.com', message: 'Failure');
+final tContactFormDtoE =
+    ContactFormDto(email: 'error@test.com', message: 'Error');
