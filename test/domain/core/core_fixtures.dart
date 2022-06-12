94ac4c7dd9315ca474f237372a7fc9fdf37828d5
Saksham Gupta <sakshamgupta391@gmail.com>
2022-06-12 22:58:33 +0530

    100% coverage for domain/core/errors.dart

--- test/domain/core/core_fixtures.dart
+++ test/domain/core/core_fixtures.dart
@@ -1,2 +1,17 @@
+import 'package:collaction_app/domain/core/value_failures.dart';
+import 'package:collaction_app/domain/core/value_objects.dart';
+import 'package:dartz/dartz.dart';
+
+class MockValueObjects extends ValueObject {
+  @override
+  // TODO: implement value
+  Either<ValueFailure, dynamic> get value => throw UnimplementedError();
+}
+
 const inputEmptyString = '';
 const inputString = 'testString';
+
+const explanation =
+    "Encountered an error at an unrecoverable point.Terminating.";
+
+const failure = ValueFailure.invalidEmail(failedValue: 'inValidEmail');
