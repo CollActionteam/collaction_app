1509dd4ed0a01ee2d275eeac8b604a1850b9863d
Isaac Obella <wizlif.144@gmail.com>
2022-09-29 09:14:44 +0300

    fix: Further simplify code cov script.

--- .github/scripts/coverage_gen.sh
+++ .github/scripts/coverage_gen.sh
@@ -5,6 +5,6 @@ touch file
 {
   printf "// Helper file to make coverage report for all dart files\n"
   echo "// ignore_for_file: unused_import"
-  find lib -type f \( -iname "*.dart" ! -iname "*.g.dart" ! -iname "*.gr.dart" ! -iname "*.freezed.dart" ! -iname "generated_plugin_registrant.dart" ! -iname "*_state.dart" ! -iname "*_event.dart" \) -print0 | xargs -0 grep -L "^part of" | cut -c 4- | sort | awk -v package="$1" '{printf "import '\''package:collaction_app%s%s'\'';\n", package, $1}'
+  find lib -type f \( -iname "*.dart" ! -iname "generated_plugin_registrant.dart" \) -print0 | xargs -0 grep -L "^part of" | cut -c 4- | sort | awk -v package="$1" '{printf "import '\''package:collaction_app%s%s'\'';\n", package, $1}'
   echo "void main(){}"
 } >>$file
