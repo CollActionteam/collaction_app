2294091f7b82f1e1012bd2747428d10b06a7ce1f
Williamson <williamson@Williamsons-MacBook-Pro.local>
2022-11-10 11:04:28 -0600

    added location dto fixtures file and location dto test file

---
+++ test/infrastructure/crowdaction/location_dto_fixtures.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('location dto fromJSON and toDOMAIN test', () {
+    test('fromJSON test', () {
+      // test here
+    });
+    test('toDOMAIN test', () {
+      // test here
+    });
+  });
+}
---
+++ test/infrastructure/crowdaction/location_dto_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('location dto fromJSON and toDOMAIN unit test', () {
+    test('location fromJSON test', () {
+      // fromJSON unit test
+    });
+    test('location toDOMAIN test', () {
+      // toDOMAIN unit test
+    });
+  });
+}
