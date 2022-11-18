8cda8fe778e3d67e201ee2c7e3071e1e7b6837b8
Williamson <williamson@Williamsons-MacBook-Pro.local>
2022-11-18 11:35:48 -0600

    added top participant dto fixtures file and top participant dto test file

---
+++ test/infrastructure/crowdaction/top_participant_fixtures.dart
@@ -0,0 +1,20 @@
+import 'package:collaction_app/domain/crowdaction/crowdaction.dart';
+import 'package:collaction_app/infrastructure/crowdaction/crowdaction_dto.dart';
+
+final tTopParticipant = TopParticipant(
+  userId: 'userId',
+  name: 'name',
+  imageUrl: 'imageUrl',
+);
+
+final tTopParticipantDto = TopParticipantDto(
+  userID: 'userId',
+  name: 'name',
+  imageUrl: 'imageUrl',
+);
+
+final tTopParticipantJson = {
+  'userId': 'userID',
+  'name': 'name',
+  'imageUrl': 'imageUrl',
+};
