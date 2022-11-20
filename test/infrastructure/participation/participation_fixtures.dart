04954a20e335db414be0db5c277c439016a975b8
Williamson <williamson@Williamsons-MacBook-Pro.local>
2022-11-20 14:07:10 -0600

    created participation fixtures file

---
+++ test/infrastructure/participation/participation_fixtures.dart
@@ -0,0 +1,37 @@
+import 'dart:ffi';
+
+import 'package:collaction_app/infrastructure/participation/participation_dto.dart';
+import 'package:collaction_app/domain/participation/participation.dart';
+
+final tParticipation = Participation(
+  id: 'id',
+  crowdActionId: 'crowdActionId',
+  fullName: 'fullName',
+  avatar: 'avatar',
+  userId: 'userId',
+  commitmentOptions: 'commitmentOptions' as List<String>,
+  joinDate: 'joinDate' as DateTime,
+  dailyCheckIns: 'dailyCheckIns' as int,
+);
+
+final tParticipationDto = ParticipationDto(
+  id: 'id',
+  crowdActionId: 'crowdActionId',
+  fullName: 'fullName',
+  avatar: 'avatar',
+  userId: 'userId',
+  commitmentOptions: 'commitmentOptions' as List<String>,
+  joinDate: 'joinDate',
+  dailyCheckIns: 'dailyCheckIns' as int,
+);
+
+const tParticipationJson = {
+  'id': 'id',
+  'crowdActionId': 'crowdActionId',
+  'fullName': 'fullName',
+  'avatar': 'avatar',
+  'userId': 'userId',
+  'commitmentOptions': 'commitmentOptions',
+  'joinDate': 'joinDate',
+  'dailyCheckIns': 'dailyCheckIns',
+};
