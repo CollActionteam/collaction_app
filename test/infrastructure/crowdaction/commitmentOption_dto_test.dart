89037d9a5595793ac47a4848331e966e490aa93b
Williamson <williamson@Williamsons-MacBook-Pro.local>
2022-11-21 14:27:02 -0600

    created CommitmentOption DTO test file
    Relates: #224

---
+++ test/infrastructure/crowdaction/commitmentOption_dto_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:collaction_app/infrastructure/crowdaction/crowdaction_dto.dart';
+import 'commitmentOption_dto_fixtures.dart';
+
+void main() {
+  group('unit tests for CommitmentOptionDto', () {
+    test('toDomain() test', () {
+      final commitmentOption = tCommitmentOptionDto.toDomain();
+
+      expect(commitmentOption, tCommitmentOption);
+    });
+    test('fromJson() test', () {
+      final commitmentOptionDto =
+          CommitmentOptionDto.fromJson(tCommitmentOptionJson);
+
+      expect(commitmentOptionDto, tCommitmentOptionDto);
+    });
+  });
+}
