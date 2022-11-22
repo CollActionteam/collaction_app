dbbda58bc5669005ac275f27a2250f104db30613
Williamson <williamson@Williamsons-MacBook-Pro.local>
2022-11-22 14:59:46 -0600

    created paginated participants dto fixtures file
    Relates: #224

---
+++ test/infrastructure/participation/paginated_participants_dto_fixtures.dart
@@ -0,0 +1,35 @@
+import 'package:collaction_app/domain/participation/paginated_participations.dart';
+import 'package:collaction_app/domain/core/page_info.dart';
+import 'package:collaction_app/infrastructure/core/page_info_dto.dart';
+import 'package:collaction_app/infrastructure/participation/paginated_participations_dto.dart';
+import 'package:collaction_app/presentation/crowdaction/crowdaction_details/widgets/participants.dart';
+
+final tPaginatedParticipants = PaginatedParticipations(
+  participations: [],
+  pageInfo: PageInfo(
+    page: 0,
+    pageSize: 0,
+    totalPages: 0,
+    totalItems: 0,
+  ),
+);
+
+final tPaginatedParticipantsDto = PaginatedParticipationsDto(
+  participations: [],
+  pageInfo: PageInfoDto(
+    page: 0,
+    pageSize: 0,
+    totalPages: 0,
+    totalItems: 0,
+  ),
+);
+
+final tPaginatedParticipantsJson = {
+  'participants': [],
+  'pageInfo': PageInfo(
+    page: 0,
+    pageSize: 0,
+    totalPages: 0,
+    totalItems: 0,
+  ),
+};
