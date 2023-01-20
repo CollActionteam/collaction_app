8ee5f3da3ba6a6ccf2664d64166a81f4c844ff86
Andre Hui <34312863+andre-tm-hui@users.noreply.github.com>
2023-01-19 22:27:40 -0800

    Update test/presentation/profile/profile_tab_test.ext.dart
    Co-authored-by: Isaac Obella <wizlif@users.noreply.github.com>

--- test/presentation/profile/profile_tab_test.ext.dart
+++ test/presentation/profile/profile_tab_test.ext.dart
@@ -2,7 +2,9 @@ part of 'profile_tab_test.dart';
 
 extension WidgetX on WidgetTester {
   Future<void> pumpProfileTab(ProfileTabBloc profileTabBloc,
-      {User? user}) async {
+      {
+      User? user,
+      }) async {
     await pumpWidget(
       BlocProvider(
         create: (context) => profileTabBloc,
