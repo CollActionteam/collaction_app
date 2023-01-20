1b39a6dd256dd482adcff6f268703943949263c0
Andre Hui <andrehui09@gmail.com>
2023-01-20 13:15:11 -0800

    fix: formatting issue

--- test/presentation/profile/profile_tab_test.ext.dart
+++ test/presentation/profile/profile_tab_test.ext.dart
@@ -2,9 +2,7 @@ part of 'profile_tab_test.dart';
 
 extension WidgetX on WidgetTester {
   Future<void> pumpProfileTab(ProfileTabBloc profileTabBloc,
-      {
-      User? user,
-      }) async {
+      {User? user}) async {
     await pumpWidget(
       BlocProvider(
         create: (context) => profileTabBloc,
