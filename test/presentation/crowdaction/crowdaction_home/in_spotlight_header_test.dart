654b6ceaaa7c1b14643a482c8e566051b9e67350
erveykee <esdelros@ucsd.edu>
2023-01-21 16:09:37 -0800

    fix incorrect test

--- test/presentation/crowdaction/crowdaction_home/in_spotlight_header_test.dart
+++ test/presentation/crowdaction/crowdaction_home/in_spotlight_header_test.dart
@@ -3,6 +3,7 @@ import 'package:collaction_app/application/crowdaction/crowdaction_details/crowd
 import 'package:collaction_app/application/crowdaction/spotlight/spotlight_bloc.dart';
 import 'package:collaction_app/application/participation/top_participants/top_participants_bloc.dart';
 import 'package:collaction_app/domain/crowdaction/crowdaction_failures.dart';
+import 'package:collaction_app/presentation/crowdaction/crowdaction_home/widgets/spotlight_crowdactions/spotlight_crowdactions.dart';
 import 'package:collaction_app/presentation/crowdaction/crowdaction_home/widgets/in_spotlight_header.dart';
 import 'package:collaction_app/presentation/shared_widgets/content_placeholder.dart';
 import 'package:collaction_app/presentation/shared_widgets/crowdaction_card.dart';
@@ -89,7 +90,7 @@ void main() {
       await tester.pump();
 
       expect(find.byType(InSpotLightHeader), findsOneWidget);
-      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      expect(find.byType(SpotlightEmptyHeader), findsOneWidget);
     });
 
     testWidgets('can render spotLightCrowdActionsError state',
