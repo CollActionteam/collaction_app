2203dccfd5956f08cb50e4e7d472d63a5bd23e8c
Isaac Obella <wizlif@users.noreply.github.com>
2023-02-08 13:52:22 +0300

    fix: Webview upgrade
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

--- lib/presentation/shared_widgets/web_view_page.dart
+++ lib/presentation/shared_widgets/web_view_page.dart
@@ -5,24 +5,93 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 import 'custom_app_bars/custom_appbar.dart';
 
-class WebViewPage extends StatelessWidget {
+class WebViewPage extends StatefulWidget {
   final String url;
   final String title;
   const WebViewPage({super.key, required this.url, this.title = ""});
 
   @override
-  Widget build(BuildContext context) {
-    if (Platform.isAndroid) {
-      WebView.platform = AndroidWebView();
-    }
+  State<WebViewPage> createState() => _WebViewPageState();
+}
 
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _controller;
+  late final ValueNotifier<bool> _loading;
+  late final ValueNotifier<bool> _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = ValueNotifier(false);
+    _error = ValueNotifier(false);
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (_) => _loading.value = true,
+          onPageFinished: (_) => _loading.value = false,
+          onWebResourceError: (_) {
+            _error.value = true;
+            _loading.value = false;
+          },
+        ),
+      );
+
+    _loadUrl();
+  }
+
+  @override
+  void dispose() {
+    _loading.dispose();
+    super.dispose();
+  }
+
+  void _loadUrl() {
+    _error.value = false;
+    _controller.loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: CustomAppBar(
-        title: title,
+        title: widget.title,
         closable: true,
       ),
-      body: WebView(initialUrl: url),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _controller),
+          ValueListenableBuilder<bool>(
+            valueListenable: _loading,
+            builder: (context, loading, _) {
+              if (!loading) return const SizedBox.shrink();
+
+              return Align(child: CircularProgressIndicator());
+            },
+          ),
+          ValueListenableBuilder<bool>(
+            valueListenable: _error,
+            builder: (context, error, _) {
+              if (!error) return const SizedBox.shrink();
+
+              return Align(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("No internet connection!!"),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: _loadUrl,
+                      child: Text("Retry"),
+                    ),
+                  ],
+                ),
+              );
+            },
+          )
+        ],
+      ),
     );
   }
 }
